a5fd7747949513f6d792c6aa21f080be28df9cbf
irenehl <26irenelopez@gmail.com>
2021-07-12 18:13:34 -0600

    🎈 Adding navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { FunctionComponent } from "react";
+
+const Footer: FunctionComponent = () => {
+    return (
+        <div>
+            <h1>This is a footer</h1>
+            <Link href="" as="">
+                <a></a>
+            </Link>
+        </div>
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,39 @@
 import { FunctionComponent } from "react";
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar: FunctionComponent = () => {
-    return(
-        <div className="w-full bg-blue-400 flex justify-around py-5">
-            <h1 className="font-bold w-1/2">Pagina</h1>
-            <h3>Link 1</h3>
-            <h3>Link 2</h3>
-        </div>
+    return (
+        <nav className="w-1/2 h-screen fixed transition-all">
+            <ul className="flex flex-col h-screen justify-end items-center list-none">
+                <li className="w-full h-16 flex items-center justify-center">
+                    <a href="#" className="space-x-3 flex justify-center items-center">
+                        <Icon className="transition-all duration-300 nav-icon text-5xl" icon={faHome}/>
+                        <span className="text-white hidden">Home</span>
+                    </a>
+                </li>
+                <li className="w-full h-16 flex items-center justify-center">
+                    <a href="#" className="space-x-3 flex justify-center items-center">
+                        <Icon className="transition-all duration-300 nav-icon text-5xl" icon={faLinkedin}/>
+                        <span className="text-white hidden">LinkedIn</span>
+                    </a>
+                </li>
+                <li className="w-full h-16 flex items-center justify-center">
+                    <a href="#" className="space-x-3 flex justify-center items-center">
+                        <Icon className="transition-all duration-300 nav-icon text-5xl" icon={faInstagram}/>
+                        <span className="text-white hidden">Instagram</span>
+                    </a>
+                </li>
+                <li className="w-full h-16 flex items-center justify-center">
+                    <a href="#" className="space-x-3 flex justify-center items-center">
+                        <Icon className="transition-all duration-300 nav-icon text-5xl" icon={faGithub}/>
+                        <span className="text-white hidden">GitHub</span>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
     )
 }
 
